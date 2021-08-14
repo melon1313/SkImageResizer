8b1a0a50fc36b6fb3e3fb2e6847dcc1c38b6d621
melon1313 <10212130@gm.nttu.edu.tw>
2021-08-14 09:06:10 +0800

    優化ResizeImagesAsync() : 修改為非同步的方式

--- SkImageResizer/SKImageProcess.cs
+++ SkImageResizer/SKImageProcess.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using SkiaSharp;
@@ -53,10 +54,9 @@ namespace SkImageResizer
                 Directory.CreateDirectory(destPath);
             }
 
-            await Task.Yield();
-
             var allFiles = FindImages(sourcePath);
-            foreach (var filePath in allFiles)
+
+            var tasks = allFiles.Select(filePath => Task.Run(() => 
             {
                 var bitmap = SKBitmap.Decode(filePath);
                 var imgPhoto = SKImage.FromBitmap(bitmap);
@@ -75,7 +75,9 @@ namespace SkImageResizer
                 using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
                 using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
                 data.SaveTo(s);
-            }
+            }));
+
+            await Task.WhenAll(tasks);
         }
 
         /// <summary>
